beda204c568fbcf1383925b0a6c428bc4da2cf5d
AlexUkraineAUE <krosbond@gmail.com>
2024-04-02 10:57:41 -0500

    Created controllers for each model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'order_items/index'
+  get 'order_items/show'
+  get 'orders/index'
+  get 'orders/show'
+  get 'categories/index'
+  get 'categories/show'
+  get 'products/index'
+  get 'products/show'
+  get 'customers/index'
+  get 'customers/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
