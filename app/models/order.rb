3618efc53c7440c7da1ea2e1eed9237b90d2fe51
AlexUkraineAUE <krosbond@gmail.com>
2024-04-11 14:42:01 -0500

    Created Admin dashbord

--- app/models/order.rb
+++
@@ -1,8 +0,0 @@
-class Order < ApplicationRecord
-  belongs_to :customer
-  has_many :order_items
-  has_many :products, through: :order_items
-
-  validates :customer_id, :total_price, presence: true
-  validates :status, inclusion: { in: %w[pending completed] }
-end
--- app/models/order_item.rb
+++
@@ -1,8 +0,0 @@
-class OrderItem < ApplicationRecord
-  belongs_to :order
-  belongs_to :product
-
-  validates :order_id, :product_id, :quantity, :subtotal, presence: true
-  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :subtotal, numericality: { greater_than_or_equal_to: 0 }
-end
