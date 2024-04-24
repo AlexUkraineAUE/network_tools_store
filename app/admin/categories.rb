25884861f2b68070811dd2116f7e2cbcbb64123f
AlexUkraineAUE <krosbond@gmail.com>
2024-04-23 19:46:37 -0500

    updated cart

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register Category do
+  filter :name
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name
+  permit_params :name, :image
   #
   # or
   #
@@ -14,5 +15,14 @@ ActiveAdmin.register Category do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  form  do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions
+  end
 
 end
